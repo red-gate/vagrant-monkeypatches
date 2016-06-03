da2e524e721dbbeeb6f9f8d8a865050776e256c4
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-06-03 14:27:31 +0100

    desperate changes to make machine_index.with_index_lock not hang forever
    on our windows agents when running multiple (unrelated) vagrant processes in parallel.

--- lib/vagrant-monkeypatches/machine_index.rb
+++ lib/vagrant-monkeypatches/machine_index.rb
@@ -1,25 +1,58 @@
-require "timeout"
+require 'timeout'
 require 'vagrant/machine_index'
+require 'log4r'
 
 # Monkey patching Vagrant::MachineIndex
 module Vagrant
 
   class MachineIndex
 
+    alias_method :old_initialize, :initialize
+    def initialize(*args)
+      @logger = Log4r::Logger.new("vagrant:machine_index")
+      old_initialize(*args)
+    end
+
     protected
+
+    # Needed as it looks like File.open() can hang on windows when running multiple vagrant processes in paralleL !?
+    # vagrant 1.8.1 and Windows Server 2012R2.
+    def open_index_lock
+      lock_path = "#{@index_file}.lock"
+      f = nil
+      while f.nil? do
+        @logger.info("Opening #{lock_path}")
+        f = Timeout::timeout(1) {File.open(lock_path, "w")} rescue nil
+      end
+      f
+    end
 
     def with_index_lock
       lock_path = "#{@index_file}.lock"
-      File.open(lock_path, "w+") do |f|
-        # It seems that f.flock(File::LOCK_EX | File::LOCK_NB) can hung forever on our windows agents
-        # even though index.lock is not even locked :(. (
-        # Is it ruby ? windows ? NTFS ? Don't know, so let's add a timeout to get it to fail so that it can be tried again.
-        # (which seems to workaround the problem :/)
-        while(Timeout::timeout(0.5) { f.flock(File::LOCK_EX | File::LOCK_NB) } rescue false) === false do
+      f = open_index_lock
+      # It seems that f.flock(File::LOCK_EX | File::LOCK_NB) can sometime hung forever on our windows agents
+      # even though index.lock is sometimes not even locked :(. (
+      # Is it ruby ? windows ? NTFS ? Don't know, so let's add a timeout to get it to fail so that it can be tried again.
+      # (which seems to workaround the problem :/)
+      # Also close the file handle and reopen it in a desperate attempt to 'make it work'
+      while(Timeout::timeout(0.5) { f.flock(File::LOCK_EX | File::LOCK_NB) } rescue false) === false do
+          @logger.warn("Could not get a lock on #{lock_path}. Closing file handle")
+          f.close
+          f = nil
+          @logger.warn("Could not get a lock on #{lock_path}. Sleeping for 0.5s")
           sleep 0.5
-        end
-        yield
+          f = open_index_lock
       end
+      begin
+        yield
+      ensure
+        @logger.info("Exiting with_index_lock and closing/unlocking #{lock_path}")
+        # This should not be needed but it looks like being explicit about it might
+        # improve reliability on windows?
+        f.flock(File::LOCK_UN)
+        f.close
+      end
+
     end
 
   end
